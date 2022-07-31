5d209f61e03e7b19b729cd0a816dbb4682c50da9
Kartik Garg <kartikgarg308@gmail.com>
2022-07-31 13:01:45 +0530

    first commit

---
+++ lib/todos.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Todos extends StatefulWidget {
+  const Todos({Key? key}) : super(key: key);
+
+  @override
+  State<Todos> createState() => _TodosState();
+}
+
+class _TodosState extends State<Todos> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
